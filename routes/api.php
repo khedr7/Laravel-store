13860cf386db0e2d93b30503371be4deb52cee6c
‪Khedr Muhieddin‬‏ <82679009+khedr7@users.noreply.github.com>
2022-03-28 19:54:53 +0300

    Auth API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register',[CustomerController::class,'register']);
+Route::post('/logIn',[CustomerController::class,'logIn']);
+Route::post('/logOut',[CustomerController::class,'logOut'])->middleware('auth:sanctum');
