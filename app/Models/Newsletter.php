7ffe32a789693ab789fa90b259921992a4c9ab5d
‪Khedr Muhieddin‬‏ <82679009+khedr7@users.noreply.github.com>
2022-03-31 20:51:29 +0300

    Newsletter model and migration

---
+++ app/Models/Newsletter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Translatable\HasTranslations;
+
+class Newsletter extends Model implements HasMedia
+{
+    use HasFactory, HasTranslations, InteractsWithMedia;
+    public $translatable = ['title', 'content'];
+    protected $fillable =[
+        'title',
+        'content'
+    ];
+}
